a19eb38811cb21e86386323bfa439d9b98e41c99
Jessica Fuentes <jgabyfuentesm@gmail.com>
2025-02-12 17:09:57 -0500

    Tipos de datos en Javascript

---
+++ hola-mundo.js
@@ -0,0 +1,41 @@
+console.log('Hola, mundo feliz :)')
+//-----------------------------------------------------
+let cajaDeAndy='Woody'
+console.log(cajaDeAndy)
+//-----------------------------------------------------
+//Tipos de datos Primitivos
+//-----------------------------------------------------
+
+//String
+let nombre='Gaby'
+//number
+let edad=25
+//boolean
+let esMayorDeEdad=true
+//null
+let noHayValor=null
+//undefine
+let noDefinido=undefined
+//symbol
+let simboloUnico=Symbol('único')
+//bigint
+let numeroGrande=2n
+
+//----------------------------------------------------------
+//Tipos de datos Complejos
+//----------------------------------------------------------
+
+//Object
+let carro = {
+    marca : 'Tesla',
+    modelo : 'Model S'
+}
+//array
+let frutas=['manzana','banano','uvas']
+//function
+function saludar(){}
+//
+//
+//
+
+
