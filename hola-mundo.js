1b7d866d4acab3c460db9e2dcd20415cd3e2c58c
Jessica Fuentes <jgabyfuentesm@gmail.com>
2025-02-13 21:48:08 -0500

    Operaciones Aritmeticas Avanzadas

--- hola-mundo.js
+++ hola-mundo.js
@@ -37,5 +37,18 @@ function saludar(){}
 //
 //
 //
+//Precisiòm
+const resultado=0.1 + 0.2
+console.log(resultado)
+console.log(resultado.toFixed(1))
+console.log(resultado===0.3)
+
+//Operaciones Avanzadas
+const raizCuadrada=Math.sqrt(16)
+const valorAbsoluto=Math.abs(-7)
+const aleatorio=Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
 
 
