7ee111a52af51938a679e69f4dfcdfdb6105bbb6
Jessica <jgabyfuentesm2@gmail.com>
2025-02-18 17:14:33 -0500

    Adivina el número secreto

---
+++ adivina-un-numero.js
@@ -0,0 +1,11 @@
+const numeroSecreto = Math.floor(Math.random()*10+1)
+const numeroJugador = parseInt(prompt('Adivina el número secreto entre el 1 al 10'))
+console.log('Este es el numero con el que juegas ${numeroJugador}')
+if(numeroJugador===numeroSecreto){
+    console.log('¡FELICIDADES, ADIVINASTE EL NÚMERO SECRETO!')
+
+}else if(numeroJugador<numeroSecreto){
+    console.log('El número es demasiado bajo, intentalo de nuevo')
+}else{
+    console.log('El número es muy alto, intenta de nuevo')
+}
