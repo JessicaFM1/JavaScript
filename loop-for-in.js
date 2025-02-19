d6b01d8ab10f5e70fac9e21357c8298eb3abf763
Jessica Fuentes <jgabyfuentesm@gmail.com>
2025-02-19 16:38:10 -0500

    Loop

---
+++ loop-for-in.js
@@ -0,0 +1,16 @@
+const listaDeCompras = {
+    manzana: 5,
+    pera: 3,
+    naranja: 2,
+    uva: 1,
+};
+
+for (fruta in listaDeCompras){
+    console.log(fruta);
+}
+
+console.log("-------------------------------------------------")
+
+for(fruta in listaDeCompras){
+    console.log(`${fruta}:${listaDeCompras[fruta]}`);
+}
---
+++ loop-for-of.js
@@ -0,0 +1,4 @@
+let canasta = ["manzana","pera","naranja","uva"];
+for (fruta of canasta){
+    console.log(fruta)
+}
