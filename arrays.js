7ee1ebbb1bc2bae0703688ae27929d8c48521f36
Jessica Fuentes <jgabyfuentesm@gmail.com>
2025-02-22 16:14:20 -0500

    Introducciòn a los Arrays

---
+++ arrays.js
@@ -0,0 +1,53 @@
+//How to create Array?
+//1. new Array() or Array
+const fruits = Array('apple','banana','orange')
+console.log(fruits)
+
+const justOneNumber = Array(12)
+console.log(justOneNumber)
+
+const number = Array(1,2,3,4,5)
+console.log(number)
+
+//2.Array literalsyntax
+const oneNumber = [4]
+console.log(oneNumber)
+
+const emptyArray = []
+console.log(emptyArray)
+
+const sports = ['soccer','tennis','rugby']
+console.log(sports)
+
+//Arrays Mixtos
+const recipeIngredients = [
+    'Flour',
+    true,
+    2,
+    {
+        ingredient: 'Milk', quantity:'1 cup'
+    },
+    false
+]
+console.log(recipeIngredients)
+
+//Accessing array elements
+const firstFruit = fruits [0]
+console.log(firstFruit)
+
+//Length property
+const numberOfFruits = fruits.length
+console.log(numberOfFruits)
+
+//Mutability
+fruits.push('watermelon')
+console.log(fruits)
+
+//Immutability
+const newFruits = fruits.concat(['grape','kiwi'])
+console.log(fruits)
+console.log(newFruits)
+
+//Checking arrays with Array.isArray()
+const isArray = Array.isArray(fruits)
+console.log(isArray)
