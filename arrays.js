ef8a98c92f497316bf3d768fab3c7d2c18aa2dfd
Jessica Fuentes <jgabyfuentesm@gmail.com>
2025-02-22 16:50:42 -0500

    Array ejercicio practico

--- arrays.js
+++ arrays.js
@@ -51,3 +51,12 @@ console.log(newFruits)
 //Checking arrays with Array.isArray()
 const isArray = Array.isArray(fruits)
 console.log(isArray)
+
+console.log('----------------------------------------------')
+//Practical exercise: sum all elements of an array
+const numbersArray = [1,2,3,4,5]
+let sum = 0
+for (let i=0; i<numbersArray.length; i++){
+    sum +=numbersArray[i]
+}
+console.log(sum)
